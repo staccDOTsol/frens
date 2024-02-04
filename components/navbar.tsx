7e75625e954b619ef46d254b2417b1462b908eb7
AlexMNet <emtamaldonado@gmail.com>
2024-02-04 12:09:55 -0800

    add navbar and assets

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,14 +1,11 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Navbar() {
   const routes = [
     {
-      path: '/',
-      label: 'Welcome Page',
-    },
-    {
-      path: '/home',
-      label: 'App Home',
+      path: '/vaults',
+      label: 'Valuts',
     },
     {
       path: '/winners',
@@ -24,15 +21,33 @@ export default function Navbar() {
     },
   ];
   return (
-    <div className="flex  justify-between items-center p-6">
-      <div>Navbar</div>
-      <div className="space-x-6">
-        {routes.map((route) => (
-          <Link key={route.path} href={route.path}>
-            {route.label}
-          </Link>
-        ))}
-      </div>
+    <div className="bg-transparent">
+      <nav className="max-w-screen-2xl mx-auto flex justify-between items-center p-6">
+        <Link href="/">
+          <Image
+            src="/images/frens-logo-17.png"
+            alt="Fens Loog"
+            height={150}
+            width={150}
+          />
+        </Link>
+        <div className="flex items-center gap-6 text-lg">
+          <div className="space-x-6 hidden md:block">
+            {routes.map((route) => (
+              <Link
+                key={route.path}
+                href={route.path}
+                className="hover:text-black/70"
+              >
+                {route.label}
+              </Link>
+            ))}
+          </div>
+          <button className="bg-[#FFD33F] text-sm font-semibold py-3 px-5 rounded-full uppercase hover:bg-[#FFD33F]/80">
+            Launch App
+          </button>
+        </div>
+      </nav>
     </div>
   );
 }
